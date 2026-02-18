8ad291906efbb861db95b83b3e1e7be1dcef4613
Moshood Sanusi <sanusimoshoodabiola@gmail.com>
2026-02-18 09:29:51 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -329,15 +329,16 @@ export default function RootLayout({
           `}
         </Script>
 
+        {children}
+        <Toaster position="top-right" richColors />
+
+        
         <Script
           src="https://tovik.app/tovik.js"
           type="module"
           data-entity="a77e3f9e121f501477c182b89ecdf8ca"
           strategy="afterInteractive"
         />
-
-        {children}
-        <Toaster position="top-right" richColors />
       </body>
     </html>
   );
