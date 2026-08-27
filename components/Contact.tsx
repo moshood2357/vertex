a850a94d291c2b9cfbe77bfe4d04ce6515dc2bb1
Moshood Sanusi <sanusimoshoodabiola@gmail.com>
2026-08-27 11:52:06 +0100

    update

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -228,12 +228,12 @@ export default function Contact() {
                         <div className="mt-4">
                           <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d703582.0013323425!2d3.3293148202620153!3d6.288808691755373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6e0b6855f00996ef%3A0x4ee218fed9996184!2sVertex%20Prime%20Digital%20Services%20Limited!5e0!3m2!1sen!2sng!4v1787825601684!5m2!1sen!2sng"
-                            width="100%"
-                            height="100%"
-                            style={{ border: 0 }}
-                            allowFullScreen
+                            // width="100%"
+                            // height="100%"
+                            // style={{ border: 0 }}
+                            // allowFullScreen
                             loading="lazy"
-                            referrerPolicy="no-referrer-when-downgrade"
+                            // referrerPolicy="no-referrer-when-downgrade"
                             title="Vertex Prime Digital Services Limited Map"
                           />
                         </div>
